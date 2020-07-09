746caa11f979217329f63daab53b3dcb8b15b92a
NitinJ1996 <nitin1996j@gmail.com>
2020-07-09 23:19:46 +0530

    usecase 2

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -x
 
+wage_per_hour=20
 empCheck=$((RANDOM%2))
 if [ $empCheck -eq 1 ]
 then
 		echo "Employee Present"
+		workhours=8
 else
 		echo "Employee Absent"
+		workhours=0
 fi
+
+echo "Employee Wage: "$((wage_per_hour*workHours))
